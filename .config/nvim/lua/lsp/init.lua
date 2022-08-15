5c92b9d69e438e84a9ab00839f24403e39705d3a
Philip Tamimi-Sarnikowski <p.sarnikowski@gmail.com>
2022-08-15 19:28:29 +0200

    fix: ensure all lsp keybindings actually work

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -3,23 +3,19 @@ local servers = {
     "bashls", "terraformls"
 }
 
-local opts = {noremap = true, silent = true}
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '<leader>k', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', '<leader>j', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
-
 local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
     -- Mappings.
     local bufopts = {noremap = true, silent = true, buffer = bufnr}
+    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '<leader>k', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', '<leader>j', vim.diagnostic.goto_next, opts)
+    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', 'H', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
     vim.keymap.set('n', '<leadre>wr', vim.lsp.buf.remove_workspace_folder,
                    bufopts)
@@ -27,8 +23,6 @@ local on_attach = function(client, bufnr)
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, bufopts)
     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', '<leader>re', vim.lsp.buf.references, bufopts)
 
     vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format, bufopts)

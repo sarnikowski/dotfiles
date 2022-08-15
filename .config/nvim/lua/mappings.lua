4934132f86f38f5b2d4d41c5e988f5961553330b
Philip Tamimi-Sarnikowski <p.sarnikowski@gmail.com>
2022-08-15 19:29:44 +0200

    refactor: use map entrypoint for all mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,27 +8,23 @@ map("n", "<TAB>", [[<Cmd>BufferLineCycleNext<CR>]], {silent = true})
 map("n", "<S-TAB>", [[<Cmd>BufferLineCyclePrev<CR>]], {silent = true})
 
 -- Nvimtree
-vim.api.nvim_set_keymap("n", "<C-t>", ":NvimTreeToggle<CR>",
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>tf", ":NvimTreeFindFile<CR>",
-                        {noremap = true, silent = true})
+map("n", "<C-t>", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
+map("n", "<leader>tf", ":NvimTreeFindFile<CR>", {noremap = true, silent = true})
 
 -- Telescope
-vim.api.nvim_set_keymap("n", "<Leader>ff",
-                        [[<Cmd>lua require('telescope.builtin').find_files()<CR>]],
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>fb",
-                        [[<Cmd>lua require('telescope.builtin').buffers()<CR>]],
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>fg",
-                        [[<Cmd>lua require('telescope.builtin').git_commits()<CR>]],
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>fc",
-                        [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]],
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>fd", [[<Cmd>Telescope cder<CR>]],
-                        {noremap = true, silent = true})
+map("n", "<Leader>ff",
+    [[<Cmd>lua require('telescope.builtin').find_files()<CR>]],
+    {noremap = true, silent = true})
+map("n", "<Leader>fb", [[<Cmd>lua require('telescope.builtin').buffers()<CR>]],
+    {noremap = true, silent = true})
+map("n", "<Leader>fg",
+    [[<Cmd>lua require('telescope.builtin').git_commits()<CR>]],
+    {noremap = true, silent = true})
+map("n", "<Leader>fc",
+    [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]],
+    {noremap = true, silent = true})
+map("n", "<Leader>fd", [[<Cmd>Telescope cder<CR>]],
+    {noremap = true, silent = true})
 
 -- ISwap
-vim.api.nvim_set_keymap("n", "<Leader>sw", [[<Cmd>ISwap<CR>]],
-                        {noremap = true, silent = true})
+map("n", "<Leader>sw", [[<Cmd>ISwap<CR>]], {noremap = true, silent = true})

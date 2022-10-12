d66d79e41a94f906e7d08cbc0f9a5ea4a37d927d
Philip Tamimi-Sarnikowski <p.sarnikowski@gmail.com>
2022-10-12 12:18:04 +0200

    fix: add mason config

---
+++ .config/nvim/lua/plugins/config/mason.lua
@@ -0,0 +1,37 @@
+local present, mason = pcall(require, "mason")
+
+if not present then
+  return
+end
+
+local options = {
+  lsps = {
+    "bash-language-server",
+    "dockerfile-language-server",
+    "eslint-lsp",
+    "json-lsp",
+    "prettier",
+    "pyright",
+    "typescript-language-server",
+    "vls",
+    "yaml-language-server",
+  },
+  linters = {
+    "flake8",
+    "golangci-lint",
+  },
+  formatters = {
+    "jq",
+    "shfmt",
+    "yamlfmt",
+  },
+  max_concurrent_installers = 10
+}
+
+mason.setup(options)
+
+vim.api.nvim_create_user_command("MasonInstallAll", function()
+  vim.cmd("MasonInstall " .. table.concat(options.lsps, " "))
+  vim.cmd("MasonInstall " .. table.concat(options.linters, " "))
+  vim.cmd("MasonInstall " .. table.concat(options.formatters, " "))
+end, {})

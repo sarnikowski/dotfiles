0400ea3eae5ebe158a0790f1bd90ad3f50579e46
Philip Tamimi-Sarnikowski <pts@ante.dk>
2022-10-19 19:39:36 +0200

    feat: add diffview toogle command

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -47,3 +47,21 @@ map("n", "<leader>xx",
 map("n", "<leader>xw",
     "<Cmd>TroubleToggle workspace_diagnostics<CR>",
     {silent = true, noremap = true})
+
+function DiffviewToogle()
+    local view = require("diffview.lib").get_current_view()
+    if view then
+        vim.cmd(":DiffviewClose")
+        vim.cmd(":NvimTreeToggle")
+    else
+        if require"nvim-tree.view".is_visible() then
+            vim.cmd(":NvimTreeToggle")
+        end
+        vim.cmd(":DiffviewOpen")
+    end
+end
+
+-- Diffview
+map("n", "<leader>dv",
+    ":lua DiffviewToogle()<CR>",
+    {silent = true, noremap = true})
